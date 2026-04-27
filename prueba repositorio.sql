2231155f66e9e7d28ffa17feabb13e221c3767cd
chenaoh <cdhenao@gmail.com>
2026-04-26 19:02:58 -0600

    se agrega el la table en main

--- prueba repositorio.sql
+++ prueba repositorio.sql
@@ -8,3 +8,18 @@ Create Table Prueba
 	Precio INT
 
 );
+
+select * from Prueba
+
+insert into Prueba(NombreID, Apellido, Precio) values ('Juan', 'Hernandez', 200);
+insert into Prueba(NombreID, Apellido, Precio) values ('Joshue', 'Perez', 200);
+insert into Prueba(NombreID, Apellido, Precio) values ('Juan', 'Hernandez', 200);
+insert into Prueba(NombreID, Apellido, Precio) values ('Juan', 'Hernandez', 200);
+
+create table Productos 
+(
+Manzana VARCHAR(50),
+Arroz varchar(5)
+);
+
+select * from Productos
